fe9aab2851370fbf97a474c9d3ccded70c86479c
Kai Otte <s73016@bht-berlin.de>
2026-04-26 17:37:00 +0200

    Fixed Startup again. It sould be better with git issues

--- startup.py
+++ startup.py
@@ -2,5 +2,14 @@ import subprocess, sys
 from pathlib import Path
 
 here = Path(__file__).parent
-subprocess.run(["git", "pull"], check=True, cwd=here)
+
+# Attempt to auto-update from remote, discarding local changes to avoid merge conflicts
+try:
+    subprocess.run(["git", "fetch", "origin"], check=True, cwd=here, capture_output=True)
+    subprocess.run(["git", "reset", "--hard", "origin/main"], check=True, cwd=here, capture_output=True)
+except subprocess.CalledProcessError:
+    # If update fails, continue anyway - better to run stale code than crash
+    pass
+
+# Start the bot - if this fails, the service will restart
 subprocess.run([sys.executable, "klingel.py"], check=True, cwd=here)
